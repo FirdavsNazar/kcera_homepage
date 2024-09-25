773ba8aba2a22f185437ca9570720911d1bbaf67
Fredrick <fredrickuzb55@gmail.com>
2024-09-26 08:48:56 +0900

    updates

--- src/components/Slider/home-one/index.js
+++ src/components/Slider/home-one/index.js
@@ -35,30 +35,45 @@ const Slider = () => {
 
         return (
             <div className={'slider-area'}>
-                <SlickSlider settings={settings}>
-                    {
-                        SliderData.map(item => (
-                            <div key={item.id}>
-                                <div className="slider-item"
-                                     style={{backgroundImage: `url(${require('../../../assets/img/' + item.bg)})`}}>
-                                    <div className="container">
-                                        <div className="row">
-                                            <div className="col-xl-7">
-                                                <div className="slider-content">
-                                                    <h2>{item.title}</h2>
-                                                    <p>{item.text}</p>
-                                                    <Link to={`${process.env.PUBLIC_URL + item.btnLink}`} className="btn btn-brand">{item.btnText}</Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </SlickSlider>
+              <SlickSlider settings={settings}>
+                {SliderData.map(item => (
+                  <div key={item.id} className="slider-item">
+                    {item.bg.endsWith('.mp4') ? (
+                      // If bg is a video file, render a video tag
+                      
+                      <video src={require(`../../../assets/img/${item.bg}`)} autoPlay muted loop style={{ width: '100%', height: '100%' }} />
+                    ) : (
+                      // If bg is not a video, use the existing background image code
+                      
+                      <div
+                        className="slider-item"
+                        // style={{
+                        //   backgroundImage: `url(${require('../../../assets/img/' + item.bg)})`,
+                        // }}
+                      ></div>
+                    )}
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-xl-7">
+                          {/* <div className="slider-content">
+                            <h2>{item.title}</h2>
+                            <p>{item.text}</p>
+                            <Link
+                              to={`${process.env.PUBLIC_URL + item.btnLink}`}
+                              className="btn btn-brand"
+                            >
+                              {item.btnText}
+                            </Link>
+                          </div> */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </SlickSlider>
             </div>
-        );
+          );
+          
     }
 ;
 

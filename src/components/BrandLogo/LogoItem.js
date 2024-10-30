2ca5497afb083b4cc9bb4da440a63f3e3927c431
Fredrick <fredrickuzb55@gmail.com>
2024-10-30 15:06:25 +0900

    some_updates

--- src/components/BrandLogo/LogoItem.js
+++ src/components/BrandLogo/LogoItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 
 function LogoItem(props) {
     return (
         <div className="brand-logo-item">
-            <Link to={`${process.env.PUBLIC_URL + props.URL}`}>
-                <img src={require('../../assets/img/' + props.logoSrc)} alt="Businex-Logo"/>
-            </Link>
+            <a href={props.URL} target="_blank" rel="noopener noreferrer">
+                <img src={require(`../../assets/img/${props.logoSrc}`)} alt="Businex-Logo" />
+            </a>
         </div>
     );
 }
